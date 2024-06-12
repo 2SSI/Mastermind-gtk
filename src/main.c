1267eaf0085f8b22a7656e5be083f23e43cb1228
I2S9 <111307883+I2S9@users.noreply.github.com>
2024-06-12 23:59:42 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "mastermind.h"
+#include "stat-essai.h"
+#include "combinaison.h"
+#include "vue.h"
+#include "controleur.h"
+
+#include <gtk/gtk.h>
+
+
+
+int main(){
+	
+	gtk_init(NULL, NULL);
+
+	ctrl_t c;
+	c = ctrl_construire();
+	
+	c = ctrl_color(c);
+	
+   	gtk_widget_show_all(GTK_WIDGET(c.vue->f));
+    	g_signal_connect(G_OBJECT(c.vue->f), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+   	gtk_main();
+    
+    	lib_ctrl(&c);
+	return EXIT_SUCCESS;
+}

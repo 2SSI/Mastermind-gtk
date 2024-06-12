cce7b15d760e63760f0888af50d062f53c0cdb81
I2S9 <111307883+I2S9@users.noreply.github.com>
2024-06-12 23:58:34 +0200

    Update controleur.h

--- src/controleur.h
+++ src/controleur.h
@@ -1,10 +1,13 @@
-#ifndef
+#ifndef CONTROLEUR_H
 
-#DEFINE CONTROLEUR_H
+#define CONTROLEUR_H
 
+#include "vue.h"
+#include "mastermind.h"
+#include <gtk/gtk.h>
 
 typedef struct{
-	mastermind* m;
+	mastermind m;
 	vue_t* vue;
 }ctrl_t;
 
@@ -12,7 +15,13 @@ typedef struct{
 ctrl_t ctrl_construire();
 void lib_ctrl(ctrl_t*);
 
+ctrl_t ctrl_color(ctrl_t);
 
+int valider_choix(vue_t*, int);
+void on_valider_clicked(GtkWidget*, gpointer);
+
+void enable(vue_t*, int);
+void disable_all(vue_t*);
 
 
 

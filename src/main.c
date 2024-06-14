5350019b6bf5e446b14cf3f0e523da68e6556a06
I2S9 <111307883+I2S9@users.noreply.github.com>
2024-06-14 21:24:21 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,24 +4,21 @@
 #include "combinaison.h"
 #include "vue.h"
 #include "controleur.h"
-
 #include <gtk/gtk.h>
-
-
 
 int main(){
 	
+	ctrl_t c;
 	gtk_init(NULL, NULL);
 
-	ctrl_t c;
 	c = ctrl_construire();
 	
-	c = ctrl_color(c);
+	/*c = ctrl_color(c);*/
 	
    	gtk_widget_show_all(GTK_WIDGET(c.vue->f));
-    	g_signal_connect(G_OBJECT(c.vue->f), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(G_OBJECT(c.vue->f), "destroy", G_CALLBACK(gtk_main_quit), NULL);
    	gtk_main();
     
-    	lib_ctrl(&c);
+    lib_ctrl(&c);
 	return EXIT_SUCCESS;
 }
